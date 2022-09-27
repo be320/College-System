9ea9dd1d3cd5f69d37b999779c350af47aa6696a
Ahmed Bahaa <33236153+be320@users.noreply.github.com>
2022-09-27 10:05:12 +0200

    Added System Codes

---
+++ src/main/java/com/dataserve/utils/SystemCodes.java
@@ -0,0 +1,41 @@
+package com.dataserve.utils;
+
+public class SystemCodes {
+
+	public static enum StatusMessages {
+		
+		//200s status messages
+		OK("INFO_200_001", "Success Operation"),
+		CREATED("INFO_200_002", "Data added successfully"),
+		UPDATED("INFO_200_003", "Data updated successfully"),
+		DELETED("INFO_200_004", "Data deleted successfully"),
+		RECEIVED("INFO_200_005", "Data received successfully"),
+		
+		//400s status messages
+		REQUIRED_DATA_MISSING("ERROR_400_001", "{0} is Required"),
+		DATA_NOT_EXISTING("ERROR_400_002", "{0} with value {1} does not exist"),
+		DATA_VALUE_INVALID("ERROR_400_003", "{0} : {1} is not valid for type {2}"),
+		UNAUTHORIZED("ERROR_400_004", "You are not authorized"),
+		
+		//500s status messages
+		GENERAL_ERROR("ERROR_500_001", "General Error");
+		
+		
+		private String code, description;
+		
+		StatusMessages(String code, String description) {
+			this.code = code;
+			this.description = description;
+		}
+
+		public String getCode() {
+			return code;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+		
+		
+	}
+}
