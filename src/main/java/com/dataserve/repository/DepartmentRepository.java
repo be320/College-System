cb875078c99e45e475e761173aa92b4990cf92fc
Ahmed Bahaa <33236153+be320@users.noreply.github.com>
2022-09-28 11:05:10 +0200

    Added Department Repository

--- src/main/java/com/dataserve/repository/DepartmentRepository.java
+++ src/main/java/com/dataserve/repository/DepartmentRepository.java
@@ -1,5 +1,10 @@
 package com.dataserve.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -8,72 +13,118 @@ import javax.transaction.Transactional;
 
 import com.dataserve.dto.Response;
 import com.dataserve.entity.Department;
+import com.dataserve.entity.Student;
 import com.dataserve.utils.DBconnection;
+import com.dataserve.utils.StaticData;
 import com.dataserve.utils.SystemCodes;
 
 @Singleton
 public class DepartmentRepository {
-	
+
 	@Inject
 	DBconnection dbConnection;
-	
+
 	public void addDepartment(Department department) {
-		Response response = new Response();
+		Connection connection = dbConnection.connectToDatabase();
+		String sql = StaticData.QUERY_DEPARTMENT_ADD;
+		PreparedStatement statement;
 		try {
-			return response;
-		}
-		catch(Exception ex) {	
-			response.setCode(SystemCodes.StatusMessages.GENERAL_ERROR.getCode());
-			response.setDescription(SystemCodes.StatusMessages.GENERAL_ERROR.getDescription());
-			return response;
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, department.getName());
+
+			int rowsInserted = statement.executeUpdate();
+			if (rowsInserted > 0) {
+				System.out.println("A new department was inserted successfully!");
+			}
+			dbConnection.closeConnection();
+		} catch (SQLException e) {
+			dbConnection.closeConnection();
+			e.printStackTrace();
 		}
 	}
 
 	public Department getDepartmentById(Integer id) {
-		Response response = new Response();
+		Department response = new Department();
+		Connection connection = dbConnection.connectToDatabase();
+		String sql = StaticData.QUERY_DEPARTMENT_GET;
+		PreparedStatement statement;
 		try {
+			statement = connection.prepareStatement(sql);
+			statement.setInt(1, id);
+			ResultSet result = statement.executeQuery(sql);
+			while (result.next()) {
+				response.setDepartmentId(result.getInt("departmentId"));
+				response.setName(result.getString("name"));
+			}
+			dbConnection.closeConnection();
 			return response;
-		}
-		catch(Exception ex) {	
-			response.setCode(SystemCodes.StatusMessages.GENERAL_ERROR.getCode());
-			response.setDescription(SystemCodes.StatusMessages.GENERAL_ERROR.getDescription());
-			return response;
+		} catch (SQLException e) {
+			dbConnection.closeConnection();
+			e.printStackTrace();
+			return null;
 		}
 	}
 
 	public void editDepartment(Department department) {
-		Response response = new Response();
+		Connection connection = dbConnection.connectToDatabase();
+		String sql = StaticData.QUERY_DEPARTMENT_EDIT;
+		Department oldData = getDepartmentById(department.getDepartmentId());
+		if (department.getName() == null)
+			department.setName(oldData.getName());
+		PreparedStatement statement;
 		try {
-			return response;
-		}
-		catch(Exception ex) {	
-			response.setCode(SystemCodes.StatusMessages.GENERAL_ERROR.getCode());
-			response.setDescription(SystemCodes.StatusMessages.GENERAL_ERROR.getDescription());
-			return response;
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, department.getName());
+			statement.setInt(2, department.getDepartmentId());
+			int rowsUpdated = statement.executeUpdate();
+			if (rowsUpdated > 0) {
+				System.out.println("An existing department was updated successfully!");
+			}
+			dbConnection.closeConnection();
+		} catch (SQLException e) {
+			dbConnection.closeConnection();
+			e.printStackTrace();
 		}
 	}
 
 	public void deleteDepartmentById(Integer id) {
-		Response response = new Response();
+		Connection connection = dbConnection.connectToDatabase();
+		String sql = StaticData.QUERY_DEPARTMENT_DELETE;
+		PreparedStatement statement;
 		try {
-			return response;
-		}
-		catch(Exception ex) {	
-			response.setCode(SystemCodes.StatusMessages.GENERAL_ERROR.getCode());
-			response.setDescription(SystemCodes.StatusMessages.GENERAL_ERROR.getDescription());
-			return response;
+			statement = connection.prepareStatement(sql);
+			statement.setInt(1, id);
+			int rowsDeleted = statement.executeUpdate();
+			if (rowsDeleted > 0) {
+				System.out.println("A department was deleted successfully!");
+			}
+			dbConnection.closeConnection();
+		} catch (SQLException e) {
+			dbConnection.closeConnection();
+			e.printStackTrace();
 		}
 	}
 
 	public List<Department> getAllDepartments() {
-		Response response = new Response();
+		List<Department> response = new ArrayList<Department>();
+		Connection connection = dbConnection.connectToDatabase();
+		String sql = StaticData.QUERY_DEPARTMENT_GET_ALL;
+		PreparedStatement statement;
 		try {
+			statement = connection.prepareStatement(sql);
+			ResultSet result = statement.executeQuery(sql);
+			while (result.next()) {
+				Department department = new Department();
+				department.setDepartmentId(result.getInt("departmentId"));
+				department.setName(result.getString("name"));
+				response.add(department);
+			}
+			dbConnection.closeConnection();
 			return response;
-		}
-		catch(Exception ex) {	
-			response.setCode(SystemCodes.StatusMessages.GENERAL_ERROR.getCode());
-			response.setDescription(SystemCodes.StatusMessages.GENERAL_ERROR.getDescription());
-			return response;
+		} catch (SQLException e) {
+			dbConnection.closeConnection();
+			e.printStackTrace();
+			return null;
 		}
 	}
 
